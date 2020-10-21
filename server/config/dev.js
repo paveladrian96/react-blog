b6ee0d4ebf50e32fbf72279e3bf769c08ef42363
Adrian Pavel <adrian.pavel-ext@continental-corporation.com>
2020-10-21 15:24:47 +0300

    initial commit

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://adrian999:capita999@cluster0.riodx.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
---
+++ server/index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+const {User} = require('./model/user');
+const { auth } = require("./midleware/auth")
+ 
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI,
+                {useNewUrlParser: true}).then(() => console.log("Db connected"))
+                                        .catch(err =>console.error(err));
+
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/api/user/auth", auth,  (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
+
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) =>{
+        if(err) return res.json({success: false, err})
+    
+        res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
+})
+
+app.post('/api/user/login', (req, res) =>{
+    //find the email
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user)
+            return res.json({
+                loginSuccess: false,
+                messajge: "Auth failed, email not found"
+            })
+    //compare password
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch){
+                return res.json({
+                    loginSuccess: false,
+                    message: "Wrong password"
+                })
+            }
+        })
+
+     //generate token
+
+     user.generateToken((err, user)=> {
+         if(err) return res.status(400).send(err);
+         res.cookie("x_auth", user.token)
+            .status(200)
+            .json({
+                loginSuccess: true
+            })
+     })
+    })
+
+    //generate token
+
+})
+
+
+app.get("/api/user/logout", auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, doc) =>{
+        if(err) return res.json({succes:false, err})
+        return res.status(200).send({
+            success: true
+        })
+    })
+})
+
+const port = process.env.PORT || 5000
+
+
+app.listen(port, () =>{
+    console.log(`Server running at ${port}`)
+});
